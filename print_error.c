563f5f7570a8f12a8dd60bb0309eaf34ab9cbd65
Melvin Redondo--Tanis <melvin@redondotanis.com>
2024-08-23 12:43:32 +0200

    Refactor print_error.c to include command argument in error message

--- print_error.c
+++ print_error.c
@@ -9,10 +9,10 @@ void print_error(arguments_t *args, char *msg)
 {
 	if (msg[0] == '\0')
 	{
-		printf("%s: %d: ", args->name, args->nb_commands);
+		printf("%s: %d: %s:", args->name, args->nb_commands, args->command);
 		fflush(stdout);
 		perror("");
 	}
 	else
-		printf("%s: %d: %s \n", args->name, args->nb_commands, msg);
+		printf("%s: %d: %s: %s\n", args->name, args->nb_commands, args->command, msg);
 }
