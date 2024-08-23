ceb11f0af74c91ef5f9c85b294d45c021ffea3a0
Melvin Redondo--Tanis <melvin@redondotanis.com>
2024-08-23 17:10:01 +0200

    Refactor interpreter.c to handle NULL command argument and improve file handling

--- interpreter.c
+++ interpreter.c
@@ -21,6 +21,68 @@ char *_getenv(const char *name)
 
 	return (NULL);
 }
+/**
+ * execute_command - execute the command
+ * @args: arguments
+ * @command: command to execute
+ * Return: status
+ */
+int execute_command(arguments_t *args, char *command)
+{
+	int status, fd;
+	char *_args[3], *tmp, *path, *path_token;
+	struct stat file_stat;
+
+	_args[0] = strtok(command, " "), _args[1] = strtok(NULL, " ");
+	_args[2] = NULL;
+	if (!_args[0])
+	{
+		free(args->command);
+		return (EXIT_FAILURE);
+	}
+	if (_args[0][0] == '.' && _args[0][1] == '\0')
+	{
+		print_error(args, "not enough arguments"), free(args->command);
+		return (EXIT_FAILURE);
+	}
+	if (_args[0][0] == '/' || _args[0][0] == '.')
+	{
+		fd = open(_args[0], O_RDONLY);
+		if (fd == -1)
+		{
+			print_error(args, "");
+			return (EXIT_FAILURE);
+		}
+		if (fstat(fd, &file_stat) == -1)
+		{
+			print_error(args, ""), close(fd);
+			return (EXIT_FAILURE);
+		}
+		status = execute(args, _args, args->env), free(args->command), close(fd);
+		return (status);
+	}
+	path = strdup(_getenv("PATH")), path_token = strtok(path, ":");
+	while (path_token)
+	{
+		tmp = malloc(strlen(path_token) + strlen(_args[0]) + 2);
+		if (!tmp)
+		{
+			free(args->command);
+			return (EXIT_FAILURE);
+		}
+		strcpy(tmp, path_token), strcat(tmp, "/"), strcat(tmp, _args[0]);
+		if (access(tmp, X_OK) == 0)
+		{
+			_args[0] = tmp;
+			status = execute(args, _args, args->env);
+			free(tmp), free(args->command), free(path);
+			return (status);
+		}
+		free(tmp), path_token = strtok(NULL, ":");
+	}
+	print_error(args, "not found"), free(args->command), free(path);
+	return (EXIT_FAILURE);
+}
 
 /**
  * interpreter - interpreter
@@ -30,84 +92,9 @@ char *_getenv(const char *name)
  */
 int interpreter(arguments_t *args)
 {
-	int status, fd;
-	char *_args[3], *tmp, *path_token;
-	char *path = strdup(_getenv("PATH"));
-	struct stat file_stat;
-
 	args->nb_commands++;
 	if (!args || !args->command)
 		return (EXIT_SUCCESS);
 
-	_args[0] = strtok(args->command, " ");
-	_args[1] = strtok(NULL, " ");
-	_args[2] = NULL;
-	if (!_args[0])
-	{
-		free(path);
-		free(args->command);
-		return (EXIT_FAILURE);
-	}
-
-	if (_args[0][0] == '.' && _args[0][1] == '\0')
-	{
-		print_error(args, "not enough arguments");
-		free(path);
-		free(args->command);
-		return (EXIT_FAILURE);
-	}
-
-	if (_args[0][0] == '/' || _args[0][0] == '.')
-	{
-		fd = open(_args[0], O_RDONLY);
-		if (fd == -1)
-		{
-			print_error(args, "");
-			return (EXIT_FAILURE);
-		}
-
-		if (fstat(fd, &file_stat) == -1)
-		{
-			print_error(args, "");
-			close(fd);
-			return (EXIT_FAILURE);
-		}
-
-		status = execute(args, _args, args->env);
-		free(path);
-		free(args->command);
-		close(fd);
-		return (status);
-	}
-
-	path_token = strtok(path, ":");
-	while (path_token)
-	{
-		tmp = malloc(strlen(path_token) + strlen(_args[0]) + 2);
-		if (!tmp)
-		{
-			free(args->command);
-			return (EXIT_FAILURE);
-		}
-
-		strcpy(tmp, path_token);
-		strcat(tmp, "/");
-		strcat(tmp, _args[0]);
-		if (access(tmp, X_OK) == 0)
-		{
-			_args[0] = tmp;
-			status = execute(args, _args, args->env);
-			free(tmp);
-			free(path);
-			free(args->command);
-			return (status);
-		}
-		free(tmp);
-		path_token = strtok(NULL, ":");
-	}
-
-	print_error(args, "not found");
-	free(path);
-	free(args->command);
-	return (EXIT_FAILURE);
+	return (execute_command(args, args->command));
 }
--- print_error.c
+++ print_error.c
@@ -7,12 +7,14 @@
  */
 void print_error(arguments_t *args, char *msg)
 {
+	char *name = args->name, nb_commands = args->nb_commands;
+
 	if (msg[0] == '\0')
 	{
-		fprintf(stderr, "%s: %d: %s:", args->name, args->nb_commands, args->command);
-		fflush(stderr);
-		perror("");
+	fprintf(stderr, "%s: %d: %s:", name, args->nb_commands, args->command);
+	fflush(stderr);
+	perror("");
 	}
 	else
-		fprintf(stderr, "%s: %d: %s: %s\n", args->name, args->nb_commands, args->command, msg);
+	fprintf(stderr, "%s: %d: %s: %s\n", name, nb_commands, args->command, msg);
 }
